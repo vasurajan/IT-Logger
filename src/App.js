2c1e3f800c68a56871dc38cfd9b7f100513e0a5a
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-11-03 08:48:00 +0530

    added addbtn and addlogmodal component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React, { Fragment, useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
+import AddBtn from './components/layout/AddBtn';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddLogModal from './components/logs/AddLogModal';
 import './App.css';
 
 const App = () => {
@@ -16,6 +18,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
